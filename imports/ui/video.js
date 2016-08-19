dc2769167295598ef30497b27f43620acda3a036
luwo1701 <luke.worley@colorado.edu>
2016-08-19 16:19:17 -0600

    initial commit

--- imports/ui/video.js
+++ imports/ui/video.js
@@ -6,7 +6,7 @@ import './video.html'
 
 //document.getElementById('PlayButton').addEventListener('click',keyPress);
 
-
+/*
 function keyPress() {
     console.log("pressed");
 }
@@ -14,20 +14,51 @@ function keyPress() {
 Template.body.onCreated(function bodyOnCreated(){
   this.state = new ReactiveDict();
 });
-
-
+*/
+/*
 Template.body.events ({
-	'click .arrow-right'(event){
-		var current = document.getElementById('PlayButton');
-		if (!current){
-			current.classlist.remove('arrow-right');
-		}
-		event.currentTarget.classList.add('arrow-right-clicked')
+//	'click .arrow-right'(event){
+		
+		var current = $( "#PlayButton")
+		if (current.hasClass("arrow-right")){	
+			event.currentTarget.classList.remove('arrow-right');
+			event.currentTarget.classList.add('arrow-right-clicked');
+			var myVideo = document.getElementById('vid');
+			//myVideo.play();
+	}
+	if (current.hasClass("arrow-right-clicked")){
+		console.log("recognize");
+	}
+console.log("pressed");
+}),
 
-console.log("pressed")
+	'click .arrow-right-clicked'(event) {
+		console.log("knows it")
+		var current = document.getElementById('PlayButton')
+	current.classlist.remove('arrow-right-clicked')
+	current.classlist.add('arrow-right')
 }
+});
+*/
 
+
+$(document).ready(function(){
+	console.log("JS loaded");
+	var play = $("#PlayButton");
+	play.click(function() {
+			debugger;	
+			console.log("play pressed");			
+			play.toggleClass("clicked");
+	});
+	
+	$("#bookmarkimg").click(function() {
+		debugger;
+		console.log("console pressed");
+	});
+	
 
 });
+
+
 
 
