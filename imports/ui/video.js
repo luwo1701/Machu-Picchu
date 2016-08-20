19753db6798a8454e601e804d342b33aec83774d
luwo1701 <luke.worley@colorado.edu>
2016-08-19 19:51:13 -0600

    Fixes #1

--- imports/ui/video.js
+++ imports/ui/video.js
@@ -1,64 +1,29 @@
 // JS for main activity here
-import { Template } from 'meteor/templating';
-import { ReactiveDict } from 'meteor/reactive-dict';
-
 import './video.html'
 
-//document.getElementById('PlayButton').addEventListener('click',keyPress);
-
-/*
-function keyPress() {
-    console.log("pressed");
-}
-
-Template.body.onCreated(function bodyOnCreated(){
-  this.state = new ReactiveDict();
-});
-*/
-/*
-Template.body.events ({
-//	'click .arrow-right'(event){
-		
-		var current = $( "#PlayButton")
-		if (current.hasClass("arrow-right")){	
-			event.currentTarget.classList.remove('arrow-right');
-			event.currentTarget.classList.add('arrow-right-clicked');
-			var myVideo = document.getElementById('vid');
-			//myVideo.play();
-	}
-	if (current.hasClass("arrow-right-clicked")){
-		console.log("recognize");
-	}
-console.log("pressed");
-}),
-
-	'click .arrow-right-clicked'(event) {
-		console.log("knows it")
-		var current = document.getElementById('PlayButton')
-	current.classlist.remove('arrow-right-clicked')
-	current.classlist.add('arrow-right')
-}
-});
-*/
-
-
 $(document).ready(function(){
-	console.log("JS loaded");
-	var play = $("#PlayButton");
-	play.click(function() {
-			debugger;	
-			console.log("play pressed");			
-			play.toggleClass("clicked");
-	});
-	
-	$("#bookmarkimg").click(function() {
-		debugger;
-		console.log("console pressed");
-	});
-	
+	var play = $('#PlayButton');
+	var menu = $('.bookmarkmenu');	
+	menu.hide();	
+	play.click(function(e) {
+			if (e.target.id=="bookmarkimg"){
+				var playPosition=$("#bookmarkimg").offset();
+				var up =  playPosition.top+20;
+				//$(".bookmarkmenu").css({left: playPosition.left + 'px', top:up+'px'})
+				$(".bookmarkmenu").toggle();	
+			}
 
+			if(e.target.id == "PlayButton"){
+				play.toggleClass("clicked");
+			}
+			
+
+			if (e.target.id == "bookmarkmenu li"){
+
+				console.log("clicked list");
+			}
+	});
 });
-
 
 
 
